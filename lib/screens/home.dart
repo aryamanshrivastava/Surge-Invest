4fe421a090d72d6b9702660f544ddc73cff6cf7b
Aryaman Shrivastava <shrivastavaaryaman0207@gmail.com>
2022-02-14 22:47:42 +0530

    intro_screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
